929d2306d7691470230fa6c63450bbf68d43b402
Alex Stewart <alexstewart314@gmail.com>
2023-06-16 16:14:54 -0700

    Edited halo_mass.py and produced new plots/text files

--- halo_mass.py
+++ halo_mass.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 # set cosmology, z vals, and mass vals for plots
 cosmo = cosmology.setCosmology('planck18',persistence='r')
 z = [5.0,10.0]
-lowLim = 3.7
+lowLim = 3.3
 highLim = 13
 M = 10**np.arange(lowLim,highLim,0.01)
 
@@ -28,8 +28,24 @@ mfunc_CDM_z10 = mass_function.massFunction(M, 10, mdef = '200m', model = 'despal
 mfunc_WDM_z5 = mass_function.massFunction(M, 5, mdef = '200m', model = 'despali16', q_out = 'dndlnM', ps_args = {'model' : 'mytable', 'path' : '/Users/alexstewart/Documents/GitHub/halo_formation_project/axino20keV_2k03_pk_log.txt'})
 mfunc_WDM_z10 = mass_function.massFunction(M, 10, mdef = '200m', model = 'despali16', q_out = 'dndlnM', ps_args = {'model' : 'mytable', 'path' : '/Users/alexstewart/Documents/GitHub/halo_formation_project/axino20keV_2k03_pk_log.txt'})
 
+mfunc_HDM_z5 = mass_function.massFunction(M, 5, mdef = '200m', model = 'despali16', q_out = 'dndlnM', ps_args = {'model' : 'mytableHDM', 'path' : '/Users/alexstewart/Documents/GitHub/halo_formation_project/hot_dark_matter_pk_log.txt'})
+
+mfunc_HDM_z10 = mass_function.massFunction(M, 10, mdef = '200m', model = 'despali16', q_out = 'dndlnM', ps_args = {'model' : 'mytableHDM', 'path' : '/Users/alexstewart/Documents/GitHub/halo_formation_project/hot_dark_matter_pk_log.txt'})
+
 #plt.plot(M, mfunc_WDM_z5, '-', label = 'WDM')
 #plt.plot(M, mfunc_CDM_z5, '-', label = 'CDM')
+plt.xlim(10**(lowLim),10**(highLim))
+plt.ylim(1E-7,1E7)
+plt.xlabel(r"$M_{200m}$")
+plt.ylabel(r"$\frac{dn}{d\ln (M)}$")
+plt.title("Halo Mass Functions (z = 10)")
+plt.plot(M, mfunc_CDM_z10, '-', label = 'CDM')
+plt.plot(M, mfunc_HDM_z10, '-', label = 'HDM')
+plt.legend()
+plt.loglog()
+plt.show()
+plt.clf()
+plt.subplot(121)
 plt.plot(M, mfunc_CDM_z10, '-', label = 'CDM')
 plt.plot(M, mfunc_WDM_z10, '-', label = 'WDM')
 plt.loglog()
@@ -37,12 +53,34 @@ plt.xlim(10**(lowLim),10**(highLim))
 plt.ylim(1E-7,1E7)
 plt.xlabel(r"$M_{200m}$")
 plt.ylabel(r"$\frac{dn}{d\ln (M)}$")
-plt.title("CDM and WDM Halo Mass Functions (z = 10)")
+plt.title("Halo Mass Functions (z = 10)")
+plt.legend()
+plt.subplot(122)
+plt.plot(M, mfunc_CDM_z5, '-', label = 'CDM')
+plt.plot(M, mfunc_WDM_z5, '-', label = 'WDM')
+plt.loglog()
+plt.xlim(10**(lowLim),10**(highLim))
+plt.ylim(1E-7,1E7)
+plt.xlabel(r"$M_{200m}$")
+plt.ylabel(r"$\frac{dn}{d\ln (M)}$")
+plt.title("Halo Mass Functions (z = 5)")
+plt.legend()
+plt.show()
+plt.clf()
+
+plt.xlim(10**(lowLim),10**(highLim))
+plt.ylim(1E-7,1E7)
+plt.xlabel(r"$M_{200m}$")
+plt.ylabel(r"$\frac{dn}{d\ln (M)}$")
+plt.title("Halo Mass Functions (z = 10)")
+plt.plot(M, mfunc_CDM_z10, '-', label = 'CDM')
+plt.plot(M, mfunc_HDM_z10, '-', label = 'HDM')
 plt.legend()
 plt.show()
 
 
 # testing stuff to see if I can get the power spectrum read from the files bc above code isn't working
+
 from colossus.utils import utilities
 
 file_path = '/Users/alexstewart/Documents/GitHub/halo_formation_project/axino20keV_2k03_pk_log.txt'
@@ -53,6 +91,10 @@ P_table = cosmo.matterPowerSpectrum(k_table, **ps_args_table)
 
 Pk = cosmo.matterPowerSpectrum(k_table)
 
+file_path2 = '/Users/alexstewart/Documents/GitHub/halo_formation_project/hot_dark_matter_pk_log.txt'
+ps_args_table_HDM = dict(model = 'mytableHDM', path = file_path2)
+PkHDM = cosmo.matterPowerSpectrum(k_table, **ps_args_table_HDM)
+
 plt.figure()
 plt.loglog()
 plt.title("Power Spectra for Plank 18 Data and WDM")
@@ -60,6 +102,7 @@ plt.xlabel('k(h / Mpc)')
 plt.ylabel('P(k)')
 plt.plot(k_table, P_table, '-', label = 'Axino 20keV WDM')
 plt.plot(k_table, Pk, '-', label='Planck 18')
+plt.plot(k_table, PkHDM, '-', label='HDM')
 plt.legend()
 plt.show()
 
@@ -75,10 +118,20 @@ with open('/Users/alexstewart/Documents/GitHub/halo_formation_project/axino20keV
 kData = kData[4:]
 PData = PData[4:]
 
-kDataArr = np.log10(np.array(kData).astype(float))
-PDataArr = np.log10(np.array(PData).astype(float))
+kDataArrTemp = np.array(kData).astype(float)
+PDataArrTemp = np.array(PData).astype(float)
+
+kDataArr = np.zeros(len(kDataArrTemp))
+PDataArr = np.zeros(len(PDataArrTemp))
+
+for i in range(0,len(kDataArrTemp)):
+    kDataArr[i] = np.log10(kDataArrTemp[i])
+    if kDataArrTemp[i] > 1:
+        PDataArr[i] = np.log10(10**-10)
+    else:
+        PDataArr[i] = np.log10(PDataArrTemp[i])
 
 dataArr = np.vstack((kDataArr, PDataArr)).T
 
-np.savetxt("axino20keV_2k03_pk_log.txt",dataArr,delimiter='\t',newline='\n')
+np.savetxt("hot_dark_matter_pk_log.txt",dataArr,delimiter='\t',newline='\n')
 '''
